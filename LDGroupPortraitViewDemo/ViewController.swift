c37f5f29666138ce2ff63802f9a59ec0e9ceee6a
Liu wei <i@liuwei.co>
2015-04-21 22:40:14 +0800

    Initial Commit

---
+++ LDGroupPortraitViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LDGroupPortraitViewDemo
+//
+//  Created by 刘伟 on 4/21/15.
+//  Copyright (c) 2015 刘伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
